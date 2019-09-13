115a642059fa843433e48f214849873405816e9e
Grimmino <Grimmino@gmail.com>
2019-09-14 00:24:49 +0500

    start filter heroes

--- static/components/main/Grid.jsx
+++ static/components/main/Grid.jsx
@@ -12,32 +12,63 @@ export default class Grid extends React.Component {
         spells: []
     }
 
-    componentWillMount = async (e) => {
-        //const api_url = await fetch(`https://api.opendota.com/api/heroes`)
-        //const heroes = await api_url.json()
-
-        const heroesJson = heroesUrl
-        const spellsJson = spellsUrl
-        console.log(heroesJson)
-
+    componentWillMount = () => {
         this.setState({
-            heroes: heroesJson.heroes,
-            spells: spellsJson.spells
+            heroes: heroesUrl.heroes
         })
     }
 
-    showInfo = () => {
+    showInfo = (e) => {
         this.setState({
             infoShow: !this.state.infoShow
         })
+    }
+
+    sortHeroes = (filter) => {
+        const filterHero = []
+        switch (filter) {
+            case 'all':
+                this.setState({
+                    heroes: heroesUrl.heroes
+                })
+                break
+
+            case 'exp':
+                heroesUrl.heroes.map(hero => {
+                    hero.talents.lvl10.exp == true
+                        ? filterHero.push(hero)
+                        : null
+                })
+
+                this.setState({
+                    heroes: filterHero
+                })
+                break
+
+            case 'attackspeed':
+                heroesUrl.heroes.map(hero => {
+                    hero.talents.lvl10.attackspeed == true ||
+                    hero.talents.lvl15.attackspeed == true ||
+                    hero.talents.lvl20.attackspeed == true ||
+                    hero.talents.lvl25.attackspeed == true
+                        ? filterHero.push(hero)
+                        : null
+                })
+
+                this.setState({
+                    heroes: filterHero
+                })
+                break
+        }
     }
 
     render() {
         return (
             <div className="grid">
                 <div className="grid__header">
-                    <div className="grid__talant">+опыт</div>
-                    <div className="grid__talant">скорость атаки</div>
+                    <div className="grid__talant" onClick={() => this.sortHeroes('all')}>все</div>
+                    <div className="grid__talant" onClick={() => this.sortHeroes('exp')}>+опыт</div>
+                    <div className="grid__talant" onClick={() => this.sortHeroes('attackspeed')}>скорость атаки</div>
                     <div className="grid__talant">урон способностей</div>
                     <div className="grid__talant">+к силе</div>
                     <div className="grid__talant">перезарядка</div>
@@ -79,22 +110,27 @@ class HeroInfo extends Grid {
                     <div className="hero__info">
                         <div className="hero__talents">
                             <div className="hero__talents_row">
-                                <div className="hero__talents_cell">{this.props.talents.lvl10.left}</div>
-                                <div className="hero__talents_cell">{this.props.talents.lvl10.right}</div>
+                                <div className="hero__talents_title">25 уровень</div>
+                                <div className="hero__talents_cell">{this.props.talents.lvl25.left}</div>
+                                <div className="hero__talents_cell">{this.props.talents.lvl25.right}</div>
                             </div>
                             <div className="hero__talents_row">
-                                <div className="hero__talents_cell">{this.props.talents.lvl15.left}</div>
-                                <div className="hero__talents_cell">{this.props.talents.lvl15.right}</div>
-                            </div>
-                            <div className="hero__talents_row">
+                                <div className="hero__talents_title">20 уровень</div>
                                 <div className="hero__talents_cell">{this.props.talents.lvl20.left}</div>
                                 <div className="hero__talents_cell">{this.props.talents.lvl20.right}</div>
                             </div>
                             <div className="hero__talents_row">
-                                <div className="hero__talents_cell">{this.props.talents.lvl25.left}</div>
-                                <div className="hero__talents_cell">{this.props.talents.lvl25.right}</div>
+                                <div className="hero__talents_title">15 уровень</div>
+                                <div className="hero__talents_cell">{this.props.talents.lvl15.left}</div>
+                                <div className="hero__talents_cell">{this.props.talents.lvl15.right}</div>
+                            </div>
+                            <div className="hero__talents_row">
+                                <div className="hero__talents_title">10 уровень</div>
+                                <div className="hero__talents_cell">{this.props.talents.lvl10.left}</div>
+                                <div className="hero__talents_cell">{this.props.talents.lvl10.right}</div>
                             </div>
                         </div>
+                        <div className="hero__layout" onClick={this.showInfo}></div>
                     </div>
                 }
             </div>
